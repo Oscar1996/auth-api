248afc47062009449d5701de847863c37310156d
Oscar Valdivia <oscarval196@gmail.com>
2021-02-09 22:29:16 -0500

    change/travis file

--- api/src/index.ts
+++ api/src/index.ts
@@ -31,6 +31,7 @@ class App {
   private initializeMiddleware() {
     this.app.use(cors());
     this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
   }
 
   private initializeErrorMiddleware() {
