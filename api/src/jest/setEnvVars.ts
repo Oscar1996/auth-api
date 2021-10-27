73832a41d617cfa9d86ec506a9edaa48aa5dc999
Oscar Valdivia <oscarval196@gmail.com>
2021-10-27 13:44:44 -0500

    Delete setEnvVars.ts

--- api/src/jest/setEnvVars.ts
+++
@@ -1,5 +0,0 @@
-process.env.SECRET_TOKEN = 'thisanothersecrettoken';
-process.env.MONGO_USER = 'oscar';
-process.env.MONGO_PASSWORD = 'quiwi25550';
-process.env.MONGO_URI = '@nodejs-raroh.mongodb.net/shopx-test?retryWrites=true&w=majority';
-process.env.PORT = '8080';
